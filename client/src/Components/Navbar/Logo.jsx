176b79a791d89393e54a9ff8403524b3118ceafc
Himanshu Dhapola <himanshu.dhapola.work@gmail.com>
2025-03-14 11:53:34 +0530

    logo link

--- client/src/Components/Navbar/Logo.jsx
+++ client/src/Components/Navbar/Logo.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 function Logo() {
   return (
-    <div className="md:w-[150px] md:h-[82px] w-[100px] h-[50px] -ml-[30px] md:ml-[1px]">
+    <div className="md:w-[150px] md:h-[82px] w-[100px] h-[50px] -ml-[30px] md:ml-[8px]">
       <NavLink to="/">
         <img src="/assets/Frame.png" alt="" className="h-full" />
       </NavLink>
