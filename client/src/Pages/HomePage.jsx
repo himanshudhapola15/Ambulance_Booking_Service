6f9e66ede1e7df0702ea8e224ec560476a88f025
Himanshu Dhapola <himanshu.dhapola.work@gmail.com>
2025-03-15 21:43:03 +0530

    direct call feature added

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -12,7 +12,9 @@ function HomePage() {
   return (
     <div className="overflow-hidden relative">
       <div className="fixed bottom-7 right-5 z-50">
-        <IoCall className="md:w-16 md:h-16 w-9 h-9 text-offwhite rounded-full p-2 md:p-3 cursor-pointer bg-red shadow-lg" />
+        <a href="tel:+8279392252">
+          <IoCall className="md:w-16 md:h-16 w-9 h-9 text-offwhite rounded-full p-2 md:p-3 cursor-pointer bg-red shadow-lg" />
+        </a>
       </div>
       <Navbar />
       <div className="mt-[60px] md:mt-[96px]">
